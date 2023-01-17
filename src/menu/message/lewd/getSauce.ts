dff10c9d2c42136b8b4baa9aba7f545bd7dcdf03
flufl <flufftehgamer@gmail.com>
2023-01-17 23:04:06 +0700

    Check for messages with no images

--- src/menu/message/lewd/getSauce.ts
+++ src/menu/message/lewd/getSauce.ts
@@ -1,4 +1,4 @@
-import { ApplicationCommandType, TextChannel } from "discord.js";
+import { ApplicationCommandType, EmbedBuilder, TextChannel } from "discord.js";
 import { getSauce } from "../../../lib/Sauce";
 import { MessageCommand } from "../../../structures/Command";
 
@@ -11,9 +11,18 @@ export default new MessageCommand({
 		if ((interaction.channel as TextChannel).nsfw) ephemeral = false;
 
 		let link: string;
-		interaction.options.data[0].message.attachments
+		interaction.options.data[0].message.attachments.size > 0
 			? (link = interaction.options.data[0].message.attachments.first().url)
 			: (link = interaction.options.data[0].message.content);
+
+		if (!link) {
+			const noImg: EmbedBuilder = new EmbedBuilder()
+				.setColor("Red")
+				.setDescription(
+					"❌ | There's no image to be searched for! If the image is inside an embed, you can copy the image link and use `/sauce link` instead! "
+				);
+			return interaction.reply({ embeds: [noImg], ephemeral: true });
+		}
 
 		await getSauce({ interaction, link, ephemeral });
 	},
