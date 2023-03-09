65fcc850f8975902ad7e727627f04cdd41377a81
fuwaguwa <flufftehgamer@gmail.com>
2023-03-09 17:39:26 +0700

    feat: added `/panda`

---
+++ src/commands/image/panda.ts
@@ -0,0 +1,24 @@
+import SRA from "somerandomapi.js";
+import { ChatInputCommand } from "../../structures/Command";
+import { EmbedBuilder } from "discord.js";
+
+export default new ChatInputCommand({
+	name: "panda",
+	description: "Incredibly stupid and cute!",
+	cooldown: 4000,
+	category: "Image",
+	run: async ({ interaction, }) => 
+	{
+		if (!interaction.deferred) await interaction.deferReply();
+
+		const pandaEmbed: EmbedBuilder = new EmbedBuilder()
+			.setColor("Random")
+			.setImage((await SRA.animal.image({ animal: "panda", })).imgUrl)
+			.setFooter({
+				text: `Requested by ${interaction.user.tag}`,
+				iconURL: interaction.user.displayAvatarURL({ forceStatic: false, }),
+			});
+
+		await interaction.editReply({ embeds: [pandaEmbed], });
+	},
+});
