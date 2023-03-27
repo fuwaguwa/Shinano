61b4c7ec3f42d5722902d01238b783ea94bf560a
fuwaguwa <flufftehgamer@gmail.com>
2023-03-27 19:22:46 +0700

    feat: support more than embeds for pages

--- src/typings/Pages.ts
+++ src/typings/Pages.ts
@@ -2,13 +2,20 @@ import {
 	ActionRowBuilder,
 	ChatInputCommandInteraction,
 	EmbedBuilder,
+	InteractionEditReplyOptions,
+	InteractionReplyOptions,
+	MessagePayloadOption,
 	StringSelectMenuBuilder
 } from "discord.js";
 
 export interface ShinanoPaginatorOptions {
 	interaction: ChatInputCommandInteraction;
 	lastPage?: number;
-	pages: EmbedBuilder[];
+	pages?: EmbedBuilder[];
+	messagePayloads?:
+	| MessagePayloadOption[]
+	| InteractionReplyOptions[]
+	| InteractionEditReplyOptions[];
 	menu?: ActionRowBuilder<StringSelectMenuBuilder>;
 	interactorOnly: boolean;
 	time: number;
